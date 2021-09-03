5e5130bd6691c63e3e8407dd186ae65882790ecd
Igor Miheev <fathalblow@gmail.com>
2021-09-04 02:29:58 +0400

    make Chat Entity

--- src/main/java/com/simbirsoft/simchat/domain/Chat.java
+++ src/main/java/com/simbirsoft/simchat/domain/Chat.java
@@ -1,5 +1,52 @@
 package com.simbirsoft.simchat.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Chat")
 public class Chat {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long chat_id;
+	private String name;
+	private String chat_type;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private Users users;
+
+	public Chat() {
+	}
+
+	public Long getChat_id() {
+		return chat_id;
+	}
+
+	public void setChat_id(Long chat_id) {
+		this.chat_id = chat_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getChat_type() {
+		return chat_type;
+	}
+
+	public void setChat_type(String chat_type) {
+		this.chat_type = chat_type;
+	}
 
 }
