5004cbd9fa726240d9c5be55ff4ef83b3211281d
Igor Miheev <fathalblow@gmail.com>
2021-09-04 00:42:57 +0400

    init commit

---
+++ src/main/java/com/simbirsoft/simchat/SimchatApplication.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.simchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimchatApplication.class, args);
+	}
+
+}
