6ff4a171c6de3afce951426f0e06b15035408874
Igor Miheev <fathalblow@gmail.com>
2021-09-05 13:25:43 +0400

    make Role Entity

--- src/main/java/com/simbirsoft/simchat/domain/Role.java
+++ src/main/java/com/simbirsoft/simchat/domain/Role.java
@@ -1,5 +1,30 @@
 package com.simbirsoft.simchat.domain;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Role {
+
+	private Long role_id;
+	private String name;
+
+	public Role() {
+	}
+
+	public Long getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(Long role_id) {
+		this.role_id = role_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
