69a48b34f244690da3d022e3206c0cc50a01f3f4
Igor Miheev <fathalblow@gmail.com>
2021-09-10 01:47:14 +0400

    add accessdto

---
+++ src/main/java/com/simbirsoft/simchat/domain/dto/AccessDto.java
@@ -0,0 +1,47 @@
+package com.simbirsoft.simchat.domain.dto;
+
+import com.simbirsoft.simchat.domain.Access;
+
+public class AccessDto {
+
+	private Long access_id;
+	private Long user_id;
+	private Long role_id;
+
+	public static AccessDto convertToDto(Access access) {
+		AccessDto accessDto = new AccessDto(access.getAccess_id(), access.getUser().getUser_id(),
+				access.getRole().getRole_id());
+		return accessDto;
+	}
+
+	public AccessDto(Long access_id, Long user_id, Long role_id) {
+		this.access_id = access_id;
+		this.user_id = user_id;
+		this.role_id = role_id;
+	}
+
+	public Long getAccess_id() {
+		return access_id;
+	}
+
+	public void setAccess_id(Long access_id) {
+		this.access_id = access_id;
+	}
+
+	public Long getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(Long user_id) {
+		this.user_id = user_id;
+	}
+
+	public Long getRole_id() {
+		return role_id;
+	}
+
+	public void setRole_id(Long role_id) {
+		this.role_id = role_id;
+	}
+
+}
